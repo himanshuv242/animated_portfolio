acd0df948d0b1c241da9b9322370943f3d672979
Himanshu Verma <himanshuv242cp@gmail.com>
2023-01-16 19:30:02 +0530

    Home page content set

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
-import { Link } from 'react-router-dom'
-import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import './index.scss';
 
 const Home = () => {
 
@@ -9,7 +10,7 @@ const Home = () => {
             <div className="text-zone">
                 <h1> Hi, <br/> I'm
                 <img src={LogoTitle} alt="developer"/>
-                Himanshu
+                imanshu
                 <br/>
                 web developer
                 </h1>
