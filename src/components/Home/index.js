4408e198bc067165a831669a3738cf5a0d7f266a
Himanshu Verma <himanshuv242cp@gmail.com>
2023-01-16 20:05:11 +0530

    Addind animation to home page text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,33 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Home = () => {
-
+const [letterClass, setLetterClass] = useState('text-animate')
+const nameArray = ['i','m','a','n','s','h','u']
+const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.']
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1> Hi, <br/> I'm
+                <h1>
+
+                <span className={letterClass}>H</span>
+                <span className={`letterClass _12`}>i,</span>
+                     <br/>
+                <span className={`letterClass _13`}>I</span>
+                <span className={`letterClass _14`}>'m</span>
+
                 <img src={LogoTitle} alt="developer"/>
-                imanshu
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15}/>
                 <br/>
-                web developer
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={22}/>
                 </h1>
                 <h2>Frontend Developer / MERN Developer / React Native Developer </h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
