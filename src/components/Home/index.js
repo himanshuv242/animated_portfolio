1b41fb47f608c50e853e8d889c16ca32194f15ff
Himanshu Verma <himanshuv242cp@gmail.com>
2023-01-16 20:28:20 +0530

    Animation error resolved

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Home = () => {
-const [letterClass, setLetterClass] = useState('text-animate')
+const [letterClass, setLetterClass] = useState('text-animate ')
+const hi=['h','i',',']
+const im=['I','\'m']
 const nameArray = ['i','m','a','n','s','h','u']
 const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.']
+
+useEffect(() => {
+    setTimeout(()=>{
+        setLetterClass('text-animate-hover')
+    },4000)
+},[])
 
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
-
-                <span className={letterClass}>H</span>
-                <span className={`letterClass _12`}>i,</span>
+<AnimatedLetters letterClass={letterClass}
+                strArray={hi}
+                idx={10}/>
+                
                      <br/>
-                <span className={`letterClass _13`}>I</span>
-                <span className={`letterClass _14`}>'m</span>
+                     <AnimatedLetters letterClass={letterClass}
+                strArray={im}
+                idx={13}/>
 
                 <img src={LogoTitle} alt="developer"/>
                 <AnimatedLetters letterClass={letterClass}
