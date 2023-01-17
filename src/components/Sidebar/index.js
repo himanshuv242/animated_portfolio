82e51ed68f50c94deba83c24562040d3a6e9b447
Himanshu Verma <himanshuv242cp@gmail.com>
2023-01-17 11:36:18 +0530

    Sidebar media queries added

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,62 +3,84 @@ import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCode, faCodeBranch, faEnvelope, faHome, faLaptopCode, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faCode, faCodeBranch, faEnvelope, faHome, faLaptopCode, faUser,faBars,faClose } from '@fortawesome/free-solid-svg-icons'
 import {  faGithub, faHackerrank, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
 
-const Sidebar = () => (
-    <div className='nav-bar'>
-            <Link className='logo' to='/'>
-                <img className='logo-s' src={LogoS} alt="logo"/>
-                <img className='sub-logo' src={LogoSubtitle} alt="He-Man"/>
-            </Link>
-            <nav>
-                <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
-                </NavLink>
+const Sidebar = () => {
+    const [showNav, setShowNav] = useState(false);
 
-                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
-                </NavLink>
 
-                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
-                </NavLink>
-            </nav>
-            <ul>
-                <li>
-                    <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/himanshuv242/' >
-                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" rel='noreferrer' href='https://github.com/himanshuv242' >
-                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" rel='noreferrer' href='https://www.hackerrank.com/himanshuv242' >
-                        <FontAwesomeIcon icon={faHackerrank} color="#4d4d4e"/>
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" rel='noreferrer' href='https://www.codechef.com/users/himanshuv242' >
-                        <FontAwesomeIcon icon={faCode} color="#4d4d4e"/>
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" rel='noreferrer' href='https://leetcode.com/himanshuv242/' >
-                        <FontAwesomeIcon icon={faCodeBranch} color="#4d4d4e"/>
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" rel='noreferrer' href='https://auth.geeksforgeeks.org/user/himanshuv242' >
-                        <FontAwesomeIcon icon={faLaptopCode} color="#4d4d4e"/>
-                    </a>
-                </li>
-            </ul>
+    return (
+        <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img className='logo-s' src={LogoS} alt="logo"/>
+            <img className='sub-logo' src={LogoSubtitle} alt="He-Man"/>
+        </Link>
+        <nav className={showNav ? 'mobile-show' : ''}>
+            <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
 
-        </div>
-)
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+            </NavLink>
+            <FontAwesomeIcon
+            onClick={()=>setShowNav(false)}
+            icon={faClose}
+            color="#ffd700"
+            size='3x'
+            className='close-icon'
+            />
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/himanshuv242/' >
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/himanshuv242' >
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.hackerrank.com/himanshuv242' >
+                    <FontAwesomeIcon icon={faHackerrank} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.codechef.com/users/himanshuv242' >
+                    <FontAwesomeIcon icon={faCode} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://leetcode.com/himanshuv242/' >
+                    <FontAwesomeIcon icon={faCodeBranch} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://auth.geeksforgeeks.org/user/himanshuv242' >
+                    <FontAwesomeIcon icon={faLaptopCode} color="#4d4d4e"/>
+                </a>
+            </li>
+        </ul>
+        <FontAwesomeIcon 
+        onClick={()=> setShowNav(true)}
+        icon={faBars}
+        color='#ffd700'
+        size='3x'
+        className='hamburger-icon'
+        />
+
+    </div>
+    )
+}
+    
+
 
 export default Sidebar
