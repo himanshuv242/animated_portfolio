cb8589a1366d45582409217bf1cc44ad7d232695
Himanshu Verma <himanshuv242cp@gmail.com>
2023-02-09 13:38:11 +0530

    Home page logo resized

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -16,22 +16,22 @@ const Logo = () => {
         <div className=" logo-container ">
                 <div className="cubespinner">
                     <div className="face1">
-                    <img src={LogoS} alt="H" className="solid-logo" />
+                    <img src={LogoS} alt="H" className="solid-logo" style={{height:"200px"}} />
                     </div>
                     <div className="face2">
-                    <img src={LogoS} alt="H" className="solid-logo" />
+                    <img src={LogoS} alt="H" className="solid-logo" style={{height:"200px"}} />
                     </div>
                     <div className="face3">
-                    <img src={LogoS} alt="H" className="solid-logo" />
+                    <img src={LogoS} alt="H" className="solid-logo" style={{height:"200px"}} />
                     </div>
                     <div className="face4">
-                    <img src={LogoS} alt="H" className="solid-logo" />
+                    <img src={LogoS} alt="H" className="solid-logo" style={{height:"200px"}} />
                     </div>
                     <div className="face5">
-                    <img src={LogoS} alt="H" className="solid-logo" />
+                    <img src={LogoS} alt="H" className="solid-logo" style={{height:"200px"}} />
                     </div>
                     <div className="face6">
-                    <img src={LogoS} alt="H" className="solid-logo" />
+                    <img src={LogoS} alt="H" className="solid-logo" style={{height:"200px"}} />
                     </div>
                 </div>
             </div>
